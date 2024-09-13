eeb4e732e3616b945d2d68f429483b49df578fe4
Artak <artakn7@gmail.com>
2024-09-13 23:51:18 +0400

    Initial Commit

---
+++ My Family Tree/My_Family_TreeApp.swift
@@ -0,0 +1,32 @@
+//
+//  My_Family_TreeApp.swift
+//  My Family Tree
+//
+//  Created by Artak on 13.09.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct My_Family_TreeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
